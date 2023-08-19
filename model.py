8fdfc86f63a694b0e73e46616d37374ae96d547a
qaseemhussain31 <85561493+qaseemhussain31@users.noreply.github.com>
2023-08-19 19:54:41 +0500

    restruturing

--- model.py
+++ model.py
@@ -4,9 +4,9 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 
-model = tf.keras.models.load_model('model.h5')
-go_obo_path = 'go.obo'
-gp_terms_path = 'terms_file.pkl'
+model = tf.keras.models.load_model('Data files/model.h5')
+go_obo_path = 'Data files/go.obo'
+gp_terms_path = 'Data files/terms_file.pkl'
 
 # Define function to convert amino acid sequence to one-hot encoding
 def seq_to_onehot(seq):
